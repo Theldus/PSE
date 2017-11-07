821080740e5478cec2aea98d1ff27516c50a5f1a
Theldus <davidsondfgl@gmail.com>
2017-11-07 21:44:48 -0200

    NodeBoxController docs

--- src/sample/nodes/NodeBoxController.java
+++ src/sample/nodes/NodeBoxController.java
@@ -1,36 +1,65 @@
 package sample.nodes;
 
-import javafx.scene.shape.CubicCurve;
 import sample.MainController;
-import sample.Workspace.Workspace;
-import sample.util.Coordinates;
 import sample.util.Edge;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Daniel on 01/11/2017.
+ *  NodeBoxController class. This class holds the logic to
+ *  connect the lines between nodes and so on.
+ *  @author Daniel, Davidson.
+ *  @version v1.0
  */
 public class NodeBoxController {
+    /**
+     * There's only one instance per NodeBox.
+     */
     private static NodeBoxController ourInstance = new NodeBoxController();
 
+    /**
+     * Line stuff.
+     */
     List<Edge> edgeList;
     private Edge edgeTemp = null;
     public static boolean connAcc = false;
 
+    /**
+     * Gets an instance of this class, since we are using Singleton
+     * Pattern.
+     * @return Returns a NodeBoxController instance.
+     */
     public static NodeBoxController getInstance() {
         return ourInstance;
     }
 
+    /**
+     * Initializes this class.
+     */
     private NodeBoxController() {
         edgeList = new ArrayList<>();
     }
+
+    /**
+     * When a connection process starts, i.e: when the user clicks
+     * for the first time in a output/input button, we maintain a
+     * temporary edge for that, and, when the connection is successfully
+     * established, we need to add the temporary edge into a edge list
+     * and resets this pointer.
+     * Resets the temporary edge.
+     */
     private void resetEdge()
     {
         edgeTemp = null;
     }
 
+    /**
+     * This method takes care about connection between NodeBoxes
+     * and is invoked whenever a user clicks in a input/output button.
+     * @param nodeBox Clicked NodeBox.
+     * @param io Input/Output indicator.
+     */
     public void initConnection(NodeBox nodeBox, Edge.IO io){
         /* Start point. */
         if (edgeTemp == null )
@@ -62,5 +91,4 @@ public class NodeBoxController {
             }
         }
     }
-
 }
