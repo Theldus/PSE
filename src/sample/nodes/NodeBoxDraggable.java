c82e45ab19d8af76d79d0ec2a715a1b856892761
Theldus <davidsondfgl@gmail.com>
2017-11-07 21:45:02 -0200

    NodeBoxDraggable docs

--- src/sample/nodes/NodeBoxDraggable.java
+++ src/sample/nodes/NodeBoxDraggable.java
@@ -1,21 +1,40 @@
 package sample.nodes;
 
-import javafx.event.Event;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
 
-
+/**
+ * NodeBoxDraggable class. This class inherits from
+ * DraggedBehaviour and overrides the appropriate
+ * methods to work properly.
+ * @implNote Unlike the name, this class handles all
+ * mouse events, not just drag events.
+ * @author Daniel
+ * @version v1.0
+ */
 public class NodeBoxDraggable extends DraggedBehavior {
 
+    /**
+     * Initializes the class.
+     * @param component Region component.
+     */
     public NodeBoxDraggable(Region component) {
         super(component);
     }
 
+    /**
+     * Gets the NodeBox attached to this class.
+     * @return Returns NodeBox component.
+     */
     @Override
     public NodeBox cast() {
         return (NodeBox) getComponent();
     }
 
+    /**
+     * onMouseEntered event.
+     * @param event Mouse event.
+     */
     @Override
     public void onMouseEntered(MouseEvent event) {
         NodeBox nodeBox = cast();
@@ -25,6 +44,10 @@ public class NodeBoxDraggable extends DraggedBehavior {
         //nodeBox.getNode().add(nodeBox.getNode().getOutput());
     }
 
+    /**
+     * onMouseExited event.
+     * @param event Mouse event.
+     */
     @Override
     public void onMouseExited(MouseEvent event) {
         NodeBox nodeBox = cast();
