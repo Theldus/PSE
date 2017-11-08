ebe521ac21c0f85872973c90a280268c6f78b5e7
Theldus <davidsondfgl@gmail.com>
2017-11-07 22:12:05 -0200

    Edge docs

--- src/sample/util/Edge.java
+++ src/sample/util/Edge.java
@@ -6,31 +6,64 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.CubicCurve;
-import javafx.scene.shape.Shape;
-import sample.Main;
 import sample.MainController;
 import sample.nodes.NodeBox;
 import sample.nodes.NodeBoxController;
 
 /**
- * Created by Daniel on 01/11/2017.
+ *  Edge class. Deals with the line between NodeBox.
+ *  @author Daniel, Davidson.
+ *  @version v1.0
  */
 public class Edge {
+    /**
+     * Input/Output indicator.
+     */
     public enum IO{Input, Output};
 
+    /**
+     * Edge stuff.
+     */
     private NodeBox nodeBoxSource;
     private NodeBox nodeBoxTarget;
     private CubicCurve line;
     private IO lastConnection;
     private EventHandler filter;
 
+    /**
+     * Sets the NodeBox and IO indicator.
+     * @param nodeBox NodeBox
+     * @param io IO indicator.
+     */
+    public Edge(NodeBox nodeBox, IO io)
+    {
+        setEdge(nodeBox, io);
+        lastConnection = io;
+    }
+
+    /**
+     * Gets the type of the last connection. It's useful
+     * since this helps to identify input/input and output/output
+     * connections.
+     * @return Returns IO indicator.
+     */
     public IO getLastConnection() {
         return lastConnection;
     }
+
+    /**
+     * Configures the last connection.
+     * @param lastConnection Last connection into this edge.
+     */
     public void setLastConnection(IO lastConnection) {
         this.lastConnection = lastConnection;
     }
 
+    /**
+     * Sets the NodeBox accordingly with his type.
+     * @param nodeBox NodeBox.
+     * @param io IO indicator.
+     */
     public void setEdge(NodeBox nodeBox, IO io){
         if (io.equals(IO.Input))
             nodeBoxSource = nodeBox;
@@ -38,43 +71,73 @@ public class Edge {
             nodeBoxTarget = nodeBox;
     }
 
-    public Edge(NodeBox nodeBox, IO io)
-    {
-        setEdge( nodeBox,io );
-        lastConnection = io;
-    }
-
+    /**
+     * Gets the NodeBox source.
+     * @return Returns NodeBox source.
+     */
     public NodeBox getNodeBoxSource() {
         return nodeBoxSource;
     }
+
+    /**
+     * Sets the NodeBox source.
+     * @param nodeBoxSource NodeBox source.
+     */
     public void setNodeBoxSource(NodeBox nodeBoxSource) {
         this.nodeBoxSource = nodeBoxSource;
     }
 
+    /**
+     * Gets the NodeBox target.
+     * @return Returns the NodeBox target.
+     */
     public NodeBox getNodeBoxTarget() {
         return nodeBoxTarget;
     }
+
+    /**
+     * Sets the NodeBox target.
+     * @param nodeBoxTarget NodeBox target.
+     */
     public void setNodeBoxTarget(NodeBox nodeBoxTarget) {
         this.nodeBoxTarget = nodeBoxTarget;
     }
 
+    /**
+     * Gets the current line used to illustrate the connection
+     * between two NodeBox.
+     * @return Returns the line.
+     */
     public CubicCurve getLine() {
         return line;
     }
+
+    /**
+     * Sets the line to be used to connect two NodeBoxes.
+     * @param line Line.
+     */
     public void setLine(CubicCurve line) {
         this.line = line;
     }
 
+    /**
+     * setupLine. This method is invoked whenever a line is being
+     * created. Both the coordinates and the event are set here.
+     * @return Returns the line created.
+     */
     public CubicCurve setupLine()
     {
+        /* Appropriate circle. */
         Circle io = (nodeBoxSource == null) ? nodeBoxTarget.getNode().getOutput()
                 : nodeBoxSource.getNode().getInput();
 
+        /* Calculate the coordinates. */
         Coordinates coordinates = new Coordinates();
         Bounds bounds = io.localToScene( io.getBoundsInLocal() );
         coordinates.setX( (bounds.getMinX() + (bounds.getWidth()  / 2)) - 50 );
         coordinates.setY(  bounds.getMinY() + (bounds.getHeight() / 2)       );
 
+        /* Create the line and setup the properties. */
         line = new CubicCurve();
         line.setFill(Color.TRANSPARENT);
         line.setStartX   ( coordinates.getX() );
@@ -88,6 +151,11 @@ public class Edge {
         line.setStroke(Color.GREENYELLOW);
         line.setStrokeWidth(2);
 
+        /*
+         * Setup the mouse move event for the line, note
+         * that we are using the Workspace to handle the
+         * event, since we need track the Workspace.
+         */
         filter = new EventHandler<MouseEvent>() {
             private Edge.IO io = Edge.this.lastConnection;
 
@@ -115,10 +183,17 @@ public class Edge {
             }
         };
 
+        /* Adds the event filter. */
         MainController.getCurrentWorkspace().addEventFilter(MouseEvent.MOUSE_MOVED, filter);
         return line;
     }
 
+    /**
+     * establishConnection. This method as the name, establishes
+     * a connection between two nodes when the user clicks an
+     * input/output button the second time.
+     * @param io IO indicator.
+     */
     public void establishConnection(IO io)
     {
         Circle circle = null;
@@ -141,6 +216,10 @@ public class Edge {
         MainController.getCurrentWorkspace().removeEventFilter(MouseEvent.MOUSE_MOVED, filter);
     }
 
+    /**
+     * closeConnection. This method is invoke when the user does
+     * an invalid connection between two NodeBoxes.
+     */
     public void closeConnection()
     {
         /* Remove mouse move event filter. */
