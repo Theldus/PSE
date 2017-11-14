fd90a0c529186667ae14509c7efc0ad12b44821e
Theldus <davidsondfgl@gmail.com>
2017-11-14 04:38:26 -0200

    Histogram working
    TODO: Fix the Histogram title

--- src/sample/nodes/Histogram.java
+++ src/sample/nodes/Histogram.java
@@ -1,13 +1,19 @@
 package sample.nodes;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 import sample.workspace.Workspace;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * Histogram class. This is a Histogram node.
  * @author Daniel
  */
 public class Histogram extends NodeBox {
+
+    protected int[] histogram = new int[256];
 
     /**
      * Initializes the Histogram.
@@ -39,17 +45,67 @@ public class Histogram extends NodeBox {
          * we have to propagate the final processed image for our child.
          */
 
-        /*
-         * At every single change made in this node, the final image *SHOULD* be
-         * send for every output line connected into this NodeBox by the update()
-         * method.
-         */
+        setImage(image);
+        execute();
 
-        System.out.println("I received an image =)");
+        if( ! (getImage() instanceof ImageFacade) ){
+            super.update(getImage());
+        }
     }
 
+    /**
+     * Execute the Histogram algorithm.
+     * @param bi Image to be processed.
+     * @return Resulting Histogram.
+     */
+    public BufferedImage execute(BufferedImage bi){
+        Graphics2D g2d;
+        BufferedImage bImg = new BufferedImage(256, 300, BufferedImage.TYPE_INT_RGB);
+        g2d = bImg.createGraphics();
+
+        /* Clear histogram. */
+        histogram = new int[256];
+
+        /* Mounts the histogram. */
+        for (int i = 0; i < bi.getWidth(); i++) {
+            for (int j = 0; j < bi.getHeight(); j++) {
+                int color = bi.getRGB(i, j);
+                int r = (color >> 16)  & 0xFF;
+                int g = (color >>  8)  & 0xFF;
+                int b = (color & 0xFF);
+                color = (r + g + b) / 3;
+                histogram [ color ]++;
+            }
+        }
+
+        /* Gets the maximum value, to serve as threshold. */
+        int top = -1;
+        for (int i = 0; i < 256; i++)
+            if (histogram[i] > top)
+                top = histogram[i];
+
+        /* Draws a title. TODO: Fix this title. */
+        g2d.drawString("Histogram (0-255)", 0, 30);
+
+        /* Draws. */
+        for (int i = 0; i < 256; i++)
+        {
+            int startY = 300 - (300 * histogram[i]) / top;
+            int   endY = 300;
+            g2d.drawLine(i, startY, i, endY);
+        }
+
+        return bImg;
+    }
+
+    /**
+     * Execute alias: converts the input image,
+     * calls the algorithm, convert back, and
+     * stores.
+     */
     @Override
     public void execute() {
-
+        Image img = SwingFXUtils.toFXImage( execute( SwingFXUtils.fromFXImage(getImage(),null) ),null  );
+        setImage(img);
     }
 }
